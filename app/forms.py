5e53f6af32cca4f9f9e4120951d17c9cbbe99651
jacob <jacobwessmark@gmail.com>
2023-02-23 15:54:48 +0100

    almost done with register stuck in circular import

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,11 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
