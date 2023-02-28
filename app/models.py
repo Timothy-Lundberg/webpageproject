b775489a5ef35c2f9028405d8add7c298d0c23a8
jacob <jacobwessmark@gmail.com>
2023-02-28 11:58:58 +0100

    jacobs changes

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     """Sätter SQL-kolumner för hur vi ska spara våra användare i databasen"""
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -64,6 +64,15 @@ def register():
     return render_template("register.html", title="Register", form=form)
 
 
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template("user.html", user=user, posts=posts)
 
 
 
